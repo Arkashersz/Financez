971e78394bff3cf17867b515a0cebf54c5e85c25
Arkashersz <126297803+Arkashersz@users.noreply.github.com>
2023-08-04 13:36:54 -0300

    <CREATE> Extrato app

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('perfil/', include('perfil.urls'))
+    path('perfil/', include('perfil.urls')),
+    path('extrato/', include('extrato.urls'))
 ]
 
